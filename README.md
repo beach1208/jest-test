9810d7ce39ca98e202986fb37a0beeaea690b841
beach1208 <nk.bg.1208@gmail.com>
2022-02-14 14:09:03 +0900

    Fixed README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # udemy-TESTING-LIBRARY
 
-Code to accompany [React Testing Library and Jest](https://www.udemy.com/course/react-testing-library/?referralCode=0B60E8FEB40F0D159E84) course on Udemy.
+Code to accompany [React Testing Library and Jest, change the text here](https://www.udemy.com/course/react-testing-library/?referralCode=0B60E8FEB40F0D159E84) course on Udemy.
 
 Test
--- lecture-code/color-button/01-click-button-to-change-color/App.js
+++ lecture-code/color-button/01-click-button-to-change-color/App.js
@@ -3,18 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [ buttonColor, setButtonColor ] = useState('red');
-  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
-
-  const currentColor = useState
   return (
-    <div >
-      <button 
-        style={{backgroundColor: buttonColor}}
-        onClick={() => setButtonColor(newButtonColor)}>
-        Change to {newButtonColor}
-      </button>
-    </div>
+    <div />
   );
 }
 
